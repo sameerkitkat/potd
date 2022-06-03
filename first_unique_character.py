536aaa4ca75489ab1866b29634ef7817dc1b1a75
Sameer Kulkarni <sameerkitkat4.4@gmail.com>
2022-06-03 22:40:25 +0530

    adding first unique character

---
+++ first_unique_character.py
@@ -0,0 +1,19 @@
+def first_unique_character(s):
+	dic = {}
+	for c in s:
+		if c in dic:
+			dic[c] = dic[c]+1
+		else:
+			dic[c] = 1
+
+	for k,v in dic.items():
+		if v == 1:
+			return k
+
+
+def main():
+	s = "leetcode"
+	print(first_unique_character(s))
+
+if __name__ == '__main__':
+	main()
