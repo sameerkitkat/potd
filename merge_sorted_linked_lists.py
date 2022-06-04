78a3cd378587565228c756edc135c9546f92bb9f
Sameer Kulkarni <sameerkitkat4.4@gmail.com>
2022-06-04 12:37:37 +0530

    adding merge sorted linked lists

--- merge_sorted_linked_lists.py
+++ merge_sorted_linked_lists.py
@@ -1,16 +1,13 @@
-# Linked List Node
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
 
 
-# Create & Handle List operations
 class LinkedList:
     def __init__(self):
         self.head = None
 
-    # Method to display the list
     def print_list(self):
         temp = self.head
         while temp.next:
@@ -18,7 +15,6 @@ class LinkedList:
             temp = temp.next
         print(temp.data)
 
-    # Method to add element to list
     def add_to_list(self, newData):
         newNode = Node(newData)
         if self.head is None:
@@ -32,20 +28,13 @@ class LinkedList:
         last.next = newNode
 
 
-# Function to merge the lists
-# Takes two lists which are sorted
-# joins them to get a single sorted list
 def merge_lists(headA, headB):
 
-    # A dummy node to store the result
-    dummyNode = Node(0)
+   dummyNode = Node(0)
 
-    # Tail stores the last node
     tail = dummyNode
     while True:
 
-        # If any of the list gets completely empty
-        # directly join all the elements of the other list
         if headA is None:
             tail.next = headB
             break
@@ -53,8 +42,6 @@ def merge_lists(headA, headB):
             tail.next = headA
             break
 
-        # Compare the data of the lists and whichever is smaller is
-        # appended to the last of the merged list and the head is changed
         if headA.data <= headB.data:
             tail.next = headA
             headA = headA.next
@@ -62,10 +49,8 @@ def merge_lists(headA, headB):
             tail.next = headB
             headB = headB.next
 
-        # Advance the tail
         tail = tail.next
 
-    # Returns the head of the merged list
     return dummyNode.next
 
 
