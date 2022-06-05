377c8248237610fa5cf2ef89498151c6edf01ca8
Sameer Kulkarni <sameerkitkat4.4@gmail.com>
2022-06-05 16:02:04 +0530

    adding search in sorted matrix

--- search_in_sorted_matrix.py
+++ search_in_sorted_matrix.py
@@ -11,6 +11,22 @@ def search(matrix,target):
 			start += 1
 	return False
 
+	'''
+	start = 0
+	end = len(matrix)*len(matrix[0])-1
+	len_col = len(matrix[0])
+
+	while start <= end:
+		mid = (start+end)//2
+		if target == matrix[mid//len_col][mid%len_col]:
+			return [mid//len_col,mid%len_col]
+		elif target < matrix[mid//len_col][mid%len_col]:
+			end = mid - 1
+		else:
+			start = mid + 1
+	return -1
+	'''
+
 def main():
 	matrix = [[1,2,3],
 			  [4,5,6],
