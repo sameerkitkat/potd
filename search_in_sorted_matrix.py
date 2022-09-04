cb6afce742e0ff48027119f8e007ae1263e2eb00
Sameer Kulkarni <sameerkitkat4.4@gmail.com>
2022-09-04 21:44:33 +0530

    changing program for search in sorted matrix

--- search_in_sorted_matrix.py
+++ search_in_sorted_matrix.py
@@ -1,5 +1,5 @@
 def search(matrix,target):
-	start = 0
+	'''start = 0
 	end = len(matrix)-1
 
 	while start < end+1 and end > 0:
@@ -9,9 +9,9 @@ def search(matrix,target):
 			end -= 1
 		else:
 			start += 1
-	return False
+	return False'''
 
-	'''
+	
 	start = 0
 	end = len(matrix)*len(matrix[0])-1
 	len_col = len(matrix[0])
@@ -25,13 +25,13 @@ def search(matrix,target):
 		else:
 			start = mid + 1
 	return -1
-	'''
+	
 
 def main():
 	matrix = [[1,2,3],
 			  [4,5,6],
 			  [7,8,9]]
-	target = 90
+	target = 9
 	print(search(matrix,target))
 
 if __name__ == '__main__':
