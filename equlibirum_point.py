16b2db025d91bef41cc392b33c90a38e6c9a4be0
Sameer Kulkarni <sameerkitkat4.4@gmail.com>
2022-09-29 23:12:54 +0530

    adding program for longest palidnrome

--- equlibirum_point.py
+++ equlibirum_point.py
@@ -11,9 +11,6 @@ def equlibrium_point(nums):
 	for i in range(n-2,-1,-1):
 		suffix_sum[i] = suffix_sum[i+1] + nums[i+1]
 
-	print(prefix_sum)
-	print(suffix_sum)
-
 	for (i,j) in zip(prefix_sum,suffix_sum):
 		if i == j:
 			return prefix_sum.index(i)+1
@@ -23,5 +20,5 @@ def main():
 	nums = [1,3,5,2,2]
 	print(equlibrium_point(nums))
 
-if __name__ == '__main__':
+if __name__ == '__main__': 
 	main()
