5808b2e972ee7b8c930f1519fab95cefef33f426
Sameer Kulkarni <sameerkitkat4.4@gmail.com>
2023-02-02 23:33:48 +0530

    readding second largest program

--- second_largest.py
+++ second_largest.py
@@ -1,19 +1,18 @@
 def second_largest(arr):
 	first = second = float('-inf')
 
-	for i in arr:
-		if i > first:
+	for n in arr:
+		if n > first:
 			second = first
-			first = i
-		elif i > second and second != first:
-			second = i
-	return second
-
+			first = n
+		if n > second and n != first:
+			second = n 
+	return second 
 
 
 
 def main():
-	arr = [3, 6, 12, 1, 5, 8]
+	arr = [11, 3, 6, 12, 1, 5, 8, 10, -1, 1000, 2000, 999]
 	print(second_largest(arr))
 
 if __name__ == '__main__':
