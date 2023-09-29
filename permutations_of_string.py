df88d3ea686b1f99e5dd56e62ba64a0ba4bf713c
Sameer Kulkarni <sameerkitkat4.4@gmail.com>
2023-09-29 17:25:42 +0530

    adding permutations of string program

--- permutations_of_string.py
+++ permutations_of_string.py
@@ -1,10 +1,10 @@
-def permute(s,l,r):
+def permute(s, l, r):
     if l == r:
         print("".join(s))
     else:
-        for i in range(l,r):
+        for i in range(l, r):
             s[l], s[i] = s[i], s[l]
-            permute(s,l+1,r)
+            permute(s, l + 1, r)
             s[l], s[i] = s[i], s[l]
 
 
